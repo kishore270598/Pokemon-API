6c4a24986f7efcbb21cacda4af32f5c7f0d77785
kishore270598 <kk94323@gmail.com>
2024-05-09 02:35:05 +0530

    API completed

--- src/component/Search.js
+++ src/component/Search.js
@@ -2,38 +2,67 @@ import React, { useEffect, useState } from "react";
 //component
 export const Search = () => {
   const [pokemon, setPokemon] = useState({});
-  const [pokeName, setPokeName] = useState();
+  const [pokeName, setPokeName] = useState("");
   //submit button use state
   const [button, setButton] = useState(false);
-  //clear button use state
-  const [clear, setClear] = useState(false);
   //pokemon image
-  const myImage = document.getElementById("myImage");
-
-  //const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [ability, setability] = useState([]);
+  const [error, setError] = useState(false);
   const handleInputChange = (event) => {
     setPokeName(event.target.value);
-    console.log(event.target.value);
   };
-  useEffect(() => {
+  function handleReset() {
+    setPokeName("");
+    setButton(false);
+    setError(false);
+    setPokemon({});
+  }
+  function handleSearch() {
     const fetchValue = async () => {
       try {
-        const Response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon/bulbasaur"
-        );
+        const url = "https://pokeapi.co/api/v2/pokemon/";
+        console.log(url);
+        const mystring1 = pokeName.replace(/ /g, "");
+        const mystring = mystring1.toLowerCase();
+        const Response = await fetch(url + mystring);
         if (!Response.ok) {
-          throw new Error("Could not find");
+          setError(true);
+          return; //break point
         }
         const data = await Response.json();
-        console.log(data);
         setPokemon(data);
+        setButton(true);
+        setError(false);
+        //set the abilities
       } catch (error) {
         console.log(error);
         // Set the source (URL) of the image
       }
     };
-    fetchValue();
-  }, []);
+    if (pokeName) {
+      fetchValue();
+    }
+  }
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+  ///
+  //IN for loop usestate will be sync due to which the state will always holds the last value
+  // we create a let variable to take reference ability by for loop
+  // then use state to update the whole ability list
+  useEffect(() => {
+    if ("abilities" in pokemon) {
+      let abilityNames = [];
+      pokemon.abilities.map((data) => {
+        abilityNames = [...abilityNames, data.ability.name];
+      });
+      setability(abilityNames);
+      // map{
+      // setability([...ability, data.ability.name])
+      // }
+    }
+  }, [pokemon]);
+
   //function
   //viki
   //jsx code html+javascript
@@ -44,33 +73,45 @@ export const Search = () => {
         id="pokemonName"
         placeholder="Enter the pokemon Name?"
         value={pokeName}
-        onChange={handleInputChange}
+        onInput={handleInputChange}
       ></input>
       <button
-        onClick={(event) => {
-          setButton(true);
-        }}
+        onClick={
+          handleSearch
+          // handleSearch--> short hand ---> "(event) => {handleSearch(event)}""
+        }
       >
         Search
       </button>
-      <button
-        onClick={(event) => {
-          setClear(true);
-        }}
-      >
-        Clear
-      </button>
-
+      <button onClick={handleReset}>Clear</button>
       <div className="pokemon_img">
-        {button ? <img src={pokemon.sprites.back_default} /> : ""}
+        {!error ? <img src={pokemon?.sprites?.back_default} /> : ""}
+        {/* short circuit if breaks,to avoid runtime error pokemon?.sprites.back_default if its un */}
       </div>
-      <div>
-        {button ? <p>Name: {pokemon.name} </p> : ""}
-        {button ? <p>Weight: {pokemon.weight} </p> : ""}
-        {button ? <p>Height: {pokemon.height} </p> : ""}
-        {button ? <p>weight: {pokemon.weight} </p> : ""}
-        {clear ? setPokemon({}) : " "}
-      </div>
+      {error ? (
+        <p>'Pokemon Not found'</p>
+      ) : (
+        <div className="container">
+          <div>
+            {button ? (
+              <p>
+                Name: <span id="pkname">{pokemon?.name}</span>{" "}
+              </p>
+            ) : (
+              ""
+            )}
+            {button ? <p>Weight: {pokemon?.weight} </p> : ""}
+            {button ? <p>Height: {pokemon?.height} </p> : ""}
+            {button ? (
+              <p>
+                Abilities: <span id="ability">{ability.join(",")}</span>
+              </p>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
